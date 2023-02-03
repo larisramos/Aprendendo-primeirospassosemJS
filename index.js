951c5096ca90a43669804e846a4a79dff4ef94d4
Larissa Ramos <68797820+larisramos@users.noreply.github.com>
2023-02-03 04:19:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+var idade = parseInt(28);
+console.log("Minha idade é: " + idade);
+*/
+
+// CALCULANDO MEDIA IDADE
+/*
+var idadeJean = parseInt(29);
+var idadeRafa = parseInt(28);
+var idadeMarlene = parseInt(32);
+var media = parseFloat();
+
+media = ((idadeJean+idadeRafa+idadeMarlene)/3)
+
+console.log("A média da idade dos meus amigos é: "+ media.toFixed(2));
+*/
+
+//
